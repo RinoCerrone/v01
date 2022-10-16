83dc45f077facf4f61ac0aff5d5ac5ac525e0dbe
Matko <tkoma999@gmail.com>
2022-10-16 22:07:52 +0200

    Add function declarations and to_hex implementation

--- app/app.cpp
+++ app/app.cpp
@@ -3,6 +3,21 @@
 
 namespace vsite::oop::v1
 {
+    string to_hex(int a){
+        std::stringstream ss;
+        ss<<hex<<a;
+        std::string res=ss.str();
+        return res;
+    }
+    string to_exp(int a){
+
+
+    }
+    string mult_table(int a,std::ostream&){
+
+
+        
+    }
 /*
 * function implementations
 */
--- app/app.h
+++ app/app.h
@@ -1,9 +1,15 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <cstring>
+#include <sstream>
 
 namespace vsite::oop::v1
 {
+    string to_hex(int a);
+    string to_exp(int a);
+    string mult_table(int a,std::ostream&);
+    
 /*
 * function declarations
 */
