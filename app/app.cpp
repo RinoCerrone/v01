7b48211524c04df594af705fae9cc2e76399a07e
Matko <tkoma999@gmail.com>
2022-10-16 22:12:22 +0200

    std

--- app/app.cpp
+++ app/app.cpp
@@ -3,20 +3,20 @@
 
 namespace vsite::oop::v1
 {
-    string to_hex(int a){
+    std::string to_hex(int a){
         std::stringstream ss;
         ss<<hex<<a;
         std::string res=ss.str();
         return res;
     }
-    string to_exp(int a){
+    std::string to_exp(int a){
 
 
     }
-    string mult_table(int a,std::ostream&){
+    std::string mult_table(int a,std::ostream&){
 
 
-        
+
     }
 /*
 * function implementations
--- app/app.h
+++ app/app.h
@@ -6,9 +6,9 @@
 
 namespace vsite::oop::v1
 {
-    string to_hex(int a);
-    string to_exp(int a);
-    string mult_table(int a,std::ostream&);
+    std::string to_hex(int a);
+    std::string to_exp(int a);
+    std::string mult_table(int a,std::ostream&);
     
 /*
 * function declarations
